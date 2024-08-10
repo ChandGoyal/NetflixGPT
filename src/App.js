abe623498c5e961a4a2e55bf29239ab33a79d2df
ChandGoyal <chandgoyal017@gmail.com>
2024-08-11 04:02:43 +0530

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./index.css";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
+const TMDB_API = process.env.REACT_APP_TMDB_API;
+console.log(TMDB_API);
 
 function App() {
   return (
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-const TMDB_API = process.env.NEXT_PUBLIC_TMDB_API;
+const TMDB_API = process.env.REACT_APP_TMDB_API;
 
 export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
