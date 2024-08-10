dc1517c3f34109a39b342cd9915b45eaf79f22d0
ChandGoyal <chandgoyal017@gmail.com>
2024-08-11 03:46:49 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Body from "./components/Body";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 
-console.log("API Key:", process.env.NEXT_PUBLIC_TMDB_API);
-
 function App() {
   return (
     <Provider store={appStore}>
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,5 @@
+const TMDB_API = process.env.NEXT_PUBLIC_TMDB_API;
+
 export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
@@ -11,7 +13,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + process.env.NEXT_PUBLIC_TMDB_API,
+    Authorization: "Bearer " + TMDB_API,
   },
 };
 
